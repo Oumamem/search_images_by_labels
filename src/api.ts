b9da5d4ece1f0c335786e34bfa88c650b118f9b5
oumamem <oumamem@gmail.com>
2023-01-20 20:26:04 +0100

    add swagger

--- src/api.ts
+++ src/api.ts
@@ -2,10 +2,23 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import ImageRoute from "./routes/ImageRoute";
+import swaggerUi from "swagger-ui-express";
+import fs from "fs";
 
 const app = express();
 const port = process.env.PORT;
 
+//swagger definition
+let swaggerFile: any = process.cwd() + "/swagger.json";
+let swaggerData: any = fs.readFileSync(swaggerFile, "utf8");
+
+let swaggerDocument = JSON.parse(swaggerData);
+
+app.use(
+	"/api/docs",
+	swaggerUi.serve,
+	swaggerUi.setup(swaggerDocument, null, null),
+);
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
